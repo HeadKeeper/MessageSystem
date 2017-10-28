31f7bab53188bc51cc6f3faf1d39bf77a44547ab
Maxim Petrusevich <maks1464@gmail.com>
2017-10-28 20:08:14 +0300

    Added forkNode element

--- src/main/elements/forkNode.go
+++ src/main/elements/forkNode.go
@@ -4,3 +4,35 @@ type ForkNode struct {
 	FirstFork Executable
 	SecondFork Executable
 }
+
+func (forkNode *ForkNode) Execute() {
+	forkNode.FirstFork.Execute()
+	forkNode.SecondFork.Execute()
+}
+
+func (forkNode *ForkNode) GetStatistics() string {
+
+	return ""
+}
+
+func (forkNode *ForkNode) GetElementState() string {
+	var forkState string
+	forkState += forkNode.FirstFork.GetElementState()
+	forkState += forkNode.SecondFork.GetElementState()
+	return forkState
+}
+
+func (forkNode *ForkNode) CanAcceptMessage() bool {
+	if !(forkNode.FirstFork.CanAcceptMessage()) && !(forkNode.SecondFork.CanAcceptMessage()) {
+		return false
+	}
+	return true
+}
+
+func (forkNode *ForkNode) AcceptMessage() {
+	if forkNode.FirstFork.CanAcceptMessage() {
+		forkNode.FirstFork.AcceptMessage()
+	} else if forkNode.SecondFork.CanAcceptMessage() {
+		forkNode.SecondFork.AcceptMessage()
+	}
+}
